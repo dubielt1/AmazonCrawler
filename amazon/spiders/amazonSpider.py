a54cf4ebbb702bf314d6b91154832b216f04be62
Tres <dubielt1@gmail.com>
2015-06-20 16:52:53 -0500

    Now supports all countries (e.g. .in .fr .de). Need to add keyword analysis

--- amazon/spiders/amazonSpider.py
+++ amazon/spiders/amazonSpider.py
@@ -20,9 +20,27 @@ class amazonSpider(CrawlSpider): #CrawlSpider
 	name = "amazon"
 	allowed_domains = []#"amazon.com"]
 
-	base_url = "http://amazon.com/"
+	#base_url = "http://amazon.com/"
 
+	base_url = "http://amazon"
+
+	
+	country = raw_input("please enter the country-code domain name (e.g. '.com', '.fr', '.co.uk'): ")
+	base_url = base_url + country + "/"
+	
+
+	if country != ".com":
+		rules = (Rule (LinkExtractor(allow=(), restrict_xpaths=("//a[contains(@href,'ref=cm_cr_pr_btm_link_next')]")), callback='parse_foreign', follow=True, ),)
+
+	else:
+		rules = (Rule (LinkExtractor(allow=(), restrict_xpaths=(".//*[@id='cm_cr-pagination_bar']/ul/li[last()]/a")), callback='parse_item', follow=True, ),)
+		
+		
+
+	global ASIN
 	ASIN = raw_input("Enter an ASIN please: ")
+
+
 
 	product_page = base_url + "dp/" + ASIN + "/"
 	review_page = base_url + "product-reviews/" + ASIN + "/"
@@ -33,8 +51,36 @@ class amazonSpider(CrawlSpider): #CrawlSpider
 	#start_urls.append(product_page)
 	start_urls.append(review_page)
 	
-	rules = (Rule (LinkExtractor(allow=(), restrict_xpaths=(".//*[@id='cm_cr-pagination_bar']/ul/li[last()]/a")), callback='parse_item', follow=True, ),)
+	#rules = (Rule (LinkExtractor(allow=(), restrict_xpaths=(".//*[@id='cm_cr-pagination_bar']/ul/li[last()]/a")), callback='parse_item', follow=True, ),)
 
+
+	#write another function for parsing foreign coutries with seperate rules and callback
+
+	def parse_foreign(self, response):
+		items = []
+
+		item = AmazonItem()
+
+		#item['title'] = response.xpath("html/body/table/tbody/tr/td[1]/h1/div[2]/a/text()").extract()
+
+		title_path = "//a[contains(@href, " + "'/dp/" + ASIN + "/ref=cm_cr_pr_product_top'" +")]/text()"
+		#title_path = "/dp/" + ASIN
+		#item['title'] = response.xpath("//a[contains(@href, title_path)]/text()").extract()		
+		item['title'] = response.xpath(title_path).extract()
+
+		#temp = response.xpath("html/body/table/tbody/tr/td[1]/div/div/table/tbody/tr/td[1]/div/div[2]/text()").extract()
+		#Don't need these
+		#temp += response.xpath("html/body/table/tbody/tr/td[1]/div/div/table/tbody/tr/td[3]/div/div[2]").extract()
+
+		temp = response.xpath(".//*[@class='reviewText']/text()").extract()
+
+		temp = ''.join(temp)
+
+		item['reviews'] = temp
+
+		items.append(item)
+
+		return items
 
 	def parse_item(self, response): #if CrawlSpider, change name (can't override parse())
 		items = []
@@ -44,7 +90,9 @@ class amazonSpider(CrawlSpider): #CrawlSpider
 		#item['title'] = response.xpath(".//*[@id='productTitle']/text()").extract()
 		item['title'] = response.xpath(".//*[@id='cm_cr-product_info']/div/div[2]/div/div/div[2]/div[1]/span/a/text()").extract()
 
-		temp = response.xpath(".//*[@class='a-section a-spacing-small page-content page-min-width']//span[@class='a-size-base' or contains(@class, 'review-text')]/text()").extract()
+		#temp = response.xpath(".//*[@class='a-section a-spacing-small page-content page-min-width']//span[@class='a-size-base' or contains(@class, 'review-text')]/text()").extract()
+
+		temp = response.xpath(".//*//span[contains(@class, 'review-text')]/text()").extract()
 
 		temp = ''.join(temp) #Makes into a string
 
